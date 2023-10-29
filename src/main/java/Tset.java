e8444345d2111c0171d803cc4d8a3050a7e5dd11
张栩垄 <2326453534@qq.com>
2023-10-29 14:25:36 +0800

    diyici

---
+++ src/main/java/Tset.java
@@ -0,0 +1,14 @@
+/**
+ * @BelongsProject: untitled1
+ * @BelongsPackage: PACKAGE_NAME
+ * @Author: 张栩垄
+ * @CreateTime: 2023-10-29  14:20
+ * @Description: 描述
+ * @Version: 1.0
+ */
+
+public class Tset {
+    public static void main(String[] args) {
+        System.out.println(111);
+    }
+}
